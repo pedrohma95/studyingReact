042b0c1f9b22090b4341d0582787d4be190105d1
Pedro De Miranda Arthur <pedrohma95@gmail.com>
2017-09-16 13:43:46 -0700

    testing input

--- src/App.js
+++ src/App.js
@@ -3,28 +3,46 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    document.getElementById("result").textContent = this.state.value;
+    document.getElementById("name").value = "";
+  }
+   
   render() {
-    const element = <h1>Hello, world!</h1>;
-    
-    function formatName(user) {
-        return user.firstName + ' ' + user.lastName;
-    }
-    
-    const user = {
-      firstName: 'Pedro',
-      lastName: 'Henrique'
-    };
 
     return (
-      <div className="App">
+      <form onSubmit={this.handleSubmit}>
+        <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>My first project using React JS</h2>
         </div>
         <p className="App-intro">
-          <p>Hello {formatName(user)}</p>
+        <label>
+          Name:
+          <input type="text" id="name" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
         </p>
+        <div id="resultForm">
+          <p><span id="welcome">Welcome <span id="result"></span></span></p>
+        </div>
       </div>
+      </form>
     );
   }
 }
